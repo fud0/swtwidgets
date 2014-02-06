937831b9cef5ea484c0f30a73a6ce79acb057b15
mrabbi <mrabbi@users.sourceforge.net>
2014-02-06 18:51:24 +0100

    Repository refactor. First projects for the Tycho infrastructure.

---
+++ widgets/map/com.buzzcoders.yasw.widgets.map/src/com/buzzcoders/yasw/widgets/map/core/Animation.java
@@ -0,0 +1,26 @@
+/*****************************************************************************************
+ * Copyright (C) 2013 Massimo Rabbi. All rights reserved.
+ * http://www.buzzcoders.com
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Massimo Rabbi <mrabbi@users.sourceforge.net> - initial API and implementation
+ *****************************************************************************************/
+package com.buzzcoders.yasw.widgets.map.core;
+
+/**
+ * Animations that can be played on a marker.
+ * 
+ * @author Massimo Rabbi (mrabbi@users.sourceforge.net)
+ * @see Marker
+ * @see {@link https://developers.google.com/maps/documentation/javascript/reference#MarkerOptions}
+ *
+ */
+public enum Animation {
+	BOUNCE,	// Marker bounces until animation is stopped.
+	DROP	// Marker falls from the top of the map ending with a small bounce.
+}
