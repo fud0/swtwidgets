b66f6aeb3ff8bb4259fbe719e9b06903443ba6cf
mrabbi <mrabbi@users.sourceforge.net>
2014-02-21 17:30:57 +0100

    New browser function for map type update.

---
+++ widgets/map/com.buzzcoders.yasw.widgets.map/src/com/buzzcoders/yasw/widgets/map/browserfunctions/UpdateMapType.java
@@ -0,0 +1,39 @@
+/*****************************************************************************************
+ * Copyright (C) 2013 Massimo Rabbi. All rights reserved.
+ * http://www.buzzcoders.com
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Massimo Rabbi <mrabbi@users.sourceforge.net> - initial API and implementation
+ *****************************************************************************************/
+package com.buzzcoders.yasw.widgets.map.browserfunctions;
+
+import org.eclipse.swt.browser.Browser;
+
+import com.buzzcoders.yasw.widgets.map.core.MapType;
+import com.buzzcoders.yasw.widgets.map.support.JavaMapSupport;
+
+/**
+ * Browser function invoked when the type of the map is changed.
+ * 
+ * @author Massimo Rabbi (mrabbi@users.sourceforge.net)
+ *
+ */
+public class UpdateMapType extends GMapEnabledFunction {
+
+	public UpdateMapType(Browser browser, String name, JavaMapSupport mapSupport) {
+		super(browser, name, mapSupport);
+	}
+	
+	@Override
+    public Object function (Object[] arguments) {
+		MapType newMapType = MapType.fromStringID((String) arguments[0]);
+        getMapSupport().setMapType(newMapType);
+        return null;
+    }
+
+}
