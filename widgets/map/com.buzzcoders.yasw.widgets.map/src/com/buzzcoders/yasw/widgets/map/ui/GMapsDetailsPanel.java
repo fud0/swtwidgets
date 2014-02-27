1d9453645775cc24ee333574e679d3d6d4fb89d7
mrabbi <mrabbi@users.sourceforge.net>
2014-02-27 12:09:58 +0100

    Copyright header year update.

--- widgets/map/com.buzzcoders.yasw.widgets.map/src/com/buzzcoders/yasw/widgets/map/ui/GMapsDetailsPanel.java
+++ widgets/map/com.buzzcoders.yasw.widgets.map/src/com/buzzcoders/yasw/widgets/map/ui/GMapsDetailsPanel.java
@@ -1,15 +1,13 @@
-/*****************************************************************************************
- * Copyright (C) 2013 Massimo Rabbi. All rights reserved.
- * http://www.buzzcoders.com
- * 
- * This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2014 Massimo Rabbi.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     Massimo Rabbi <mrabbi@users.sourceforge.net> - initial API and implementation
- *****************************************************************************************/
+ ******************************************************************************/
 package com.buzzcoders.yasw.widgets.map.ui;
 
 import org.eclipse.jface.dialogs.MessageDialog;
